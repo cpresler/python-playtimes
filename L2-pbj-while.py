6d9766e9ce2b5212392bc19912a9afecc9962748
Christy Presler <cnpresler@gmail.com>
2016-03-21 22:18:58 -0400

    fix for 0 pb or 0 jelly

--- L2-pbj-while.py
+++ L2-pbj-while.py
@@ -1,7 +1,7 @@
 # Goal 1:
 bread = 10
-peanut_butter = 8
-jelly = 10
+peanut_butter = 6
+jelly = 3
 sandwiches = bread/2
 sandwich = 0
 
@@ -14,24 +14,25 @@ elif(sandwiches == 0):
 	print "Blarg, I've got no lunch!"
 
 
-
-while sandwiches > 0:
-	sandwich += 1
-	print 'Making sandwich #{0}'.format(sandwich)
-	sandwiches -= 1
-	# Goal 2:
-	rem_bread = bread/2 - sandwich
-	rem_jelly = jelly - sandwich
-	rem_pb = peanut_butter - sandwich
-	if(rem_bread > 0 and rem_pb > 0 and rem_jelly > 0):
-		print 'I have enough bread for {0} more sandwiches, enough peanut butter for {1} more, and enough jelly for {2} more.'.format(rem_bread, rem_pb, rem_jelly)
-	elif(rem_bread == 0):
-		print "All done; I ran out of bread"
-	elif(rem_jelly == 0):
-		print "All done; I ran out of jelly"
-	elif(rem_pb == 0):
-		print "All done; I ran out of peanut butter"
-	else:
-		print "Error, something weird happened."
-
+if(sandwiches > 0):
+	while sandwiches > 0:
+		sandwich += 1
+		print 'Making sandwich #{0}'.format(sandwich)
+		sandwiches -= 1
+		# Goal 2:
+		rem_bread = bread/2 - sandwich
+		rem_jelly = jelly - sandwich
+		rem_pb = peanut_butter - sandwich
+		if(rem_bread > 0 and rem_pb > 0 and rem_jelly > 0):
+			print 'I have enough bread for {0} more sandwiches, enough peanut butter for {1} more, and enough jelly for {2} more.'.format(rem_bread, rem_pb, rem_jelly)
+		elif(rem_bread == 0):
+			print "All done; I ran out of bread"
+		elif(rem_jelly == 0):
+			print "All done; I ran out of jelly"
+		elif(rem_pb == 0):
+			print "All done; I ran out of peanut butter"
+		else:
+			print "Error, something weird happened."
+else:
+	print "Oh no, I'm missing peanut butter or jelly"
 
